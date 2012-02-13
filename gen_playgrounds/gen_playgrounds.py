d2544f150dd097ecd4057dc31f35f619f47e3133
Sergei Zhgirovski <ixanezis@gmail.com>
2012-02-13 23:54:35 +0300

    Introducing apparmor sandbox generating script v 1.0

--- gen_playgrounds/gen_playgrounds.py
+++ gen_playgrounds/gen_playgrounds.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python2.7
+# Run this script under sudo
+from __future__ import print_function
 from subprocess import check_call
 from sys import stdin, exit
 import argparse
 import shutil
 import os
-import __future__ print_function
+
+APPARMOR_INIT = '/etc/init.d/apparmor'
+APPARMOR_PROFILES = '/etc/apparmor.d/'
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description = "Generates several playgrounds and makes apparmor use them")
   parser.add_argument("path")
-  parser.add_argument("--count", default=4, type=int)
+  parser.add_argument("--count", type=int, default=4)
   args = parser.parse_args()
   path = args.path
 
@@ -31,17 +35,23 @@ if __name__ == "__main__":
   for executable in open('copy-executables'):
     executable = executable.strip()
     if executable == '': continue
-    check_call(["cp", executable, sandbox_path])
+    shutil.copy(executable, sandbox_path)
+#    check_call(["cp", executable, sandbox_path])
 
-  profile = open('profile').read().format(os.path.join(path, '**'))
+  playground_with_subdir = os.path.join(path, '**') # all subdirectories and files
+  profile = open('profile').read().format(playground_with_subdir, playground_with_subdir)
 
   for entry in os.listdir(sandbox_path):
-    output = open(os.path.join("/etc/apparmor.d/", os.path.join(sandbox_path, entry).replace('/', '.')[1:]), 'w')
+    output = open(os.path.join(APPARMOR_PROFILES, os.path.join(sandbox_path, entry).replace('/', '.')[1:]), 'w')
     #for now, using a single profile for all binary types
     print (profile, file=output, end='')
-    #output = open(os.path.join("/etc/apparmor.d/", os.join(sandbox_path, entry).replace('/', '.')))
- 
-#  output = open("/etc/apparmor.d/
+  
+  for entry in os.listdir(path):
+    if os.path.isdir(os.path.join(path, entry)):
+      cpp_binary = os.path.join(path, entry, 'a.out') #TODO: What is a default cpp binary file ?
+      output = open(os.path.join(APPARMOR_PROFILES, cpp_binary.replace('/', '.')[1:]), 'w')
+      print(profile, file = output)
 
-
-
+  check_call([APPARMOR_INIT, 'restart'])
+  check_call(["chmod", '777', '-R',  path])
+   
