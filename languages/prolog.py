6b05815c6fe01e452fa2b19ca52ab4f8267cb1c3
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-03-11 03:01:17 +0300

    Add prolog support.

---
+++ languages/prolog.py
@@ -0,0 +1,21 @@
+from compilable_task import CompilableTask
+
+def parse_list(result):
+    items = map(lambda(st): st.strip(), result[1:-1].strip().split(','))
+    return set(items)
+
+def prolog_checker(expected, real):
+    result = real.splitlines().pop()
+    return parse_list(result) == parse_list(expected)
+
+
+class PrologTask(CompilableTask):
+    filenames = ['prolog.pl']
+    compile_string = "gplc {0}prolog.pl -o {0}prolog"
+    execute_string = "{0}prolog"
+
+    def __init__(self):
+        CompilableTask.__init__(self, checker = prolog_checker)
+
+
+
---
+++ tasks/PROLOG/0/task.py
@@ -0,0 +1,3 @@
+from languages.prolog import PrologTask
+
+task = PrologTask()
