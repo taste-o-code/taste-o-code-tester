c35d6d8885a7db7840d938d314d2c19e5d54552a
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-04-20 01:00:47 +0300

    Add clojure support.

---
+++ languages/clojure.py
@@ -0,0 +1,8 @@
+from task import Task
+
+SANDBOX_FILES_PATH = '/home/playground/sandbox-files/'
+
+class ClojureTask(Task):
+
+    execute_string = "java -Djava.security.manager -Xmx140m -Djava.security.policy={0}java.policy -cp {0}clojure-1.4.0.jar clojure.main {{0}}solution.clj".format(SANDBOX_FILES_PATH)
+    filenames = ["solution.clj"]
---
+++ tasks/CLOJURE/hello-world/task.py
@@ -0,0 +1,6 @@
+from languages.clojure import ClojureTask
+
+# Description
+# Write 'Hello, world.' to console.
+
+task = ClojureTask()
