b1008902561c0b8ee67e2b86bce03aee14a33af7
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-02-18 17:12:22 +0300

    Don't show 'Program crashed' message if user returned incorrect answer for 'reinvent hiccup' task.

--- tasks/CLOJURE/reinvent-hiccup/precode.clj
+++ tasks/CLOJURE/reinvent-hiccup/precode.clj
@@ -5,10 +5,15 @@
 (defn str-to-stream [str]
   (java.io.ByteArrayInputStream. (.getBytes str)))
 
+(defn safe-parse [content]
+  (try
+    (clojure.xml/parse content)
+    (catch java.lang.Exception e)))
+
 (defn check [to-encode expected]
   (->> [(html to-encode) expected]
        (map str-to-stream)
-       (map clojure.xml/parse)
+       (map safe-parse)
        (apply =)
        (println)))
 
