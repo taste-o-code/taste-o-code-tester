ead506ae0d20e716d3eaa433b82a139aa58bf6f3
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-03-20 17:27:14 +0300

    Reduce probability of interfering function names with user's functions.

--- tasks/CLOJURE/reinvent-hiccup/precode.clj
+++ tasks/CLOJURE/reinvent-hiccup/precode.clj
@@ -2,18 +2,17 @@
 
 (declare html)
 
-(defn str-to-stream [str]
-  (java.io.ByteArrayInputStream. (.getBytes str)))
-
-(defn safe-parse [content]
-  (try
-    (clojure.xml/parse content)
-    (catch java.lang.Exception e)))
-
-(defn check [to-encode expected]
-  (->> [(html to-encode) expected]
-       (map str-to-stream)
-       (map safe-parse)
-       (apply =)
-       (println)))
+;;; Use some random stuff at the end to reduce probability of user defining function with same name.
+(defn check-hiccup-task-48gj4Jk [to-encode expected]
+  (letfn [(str-to-stream [str]
+            (java.io.ByteArrayInputStream. (.getBytes str)))
+          (safe-parse [content]
+            (try
+              (clojure.xml/parse content)
+    (catch java.lang.Exception e)))]
+      (->> [(html to-encode) expected]
+        (map str-to-stream)
+        (map safe-parse)
+        (apply =)
+        (println))))
 
