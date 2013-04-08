79570af4646547760dc3275bf56f66564e3979a7
Nikita Beloglazov <nikelandjelo@gmail.com>
2013-04-09 02:31:23 +0300

    Use java.policy for java tasks.

--- languages/java.py
+++ languages/java.py
@@ -7,12 +7,13 @@ from tools.file_creators import create_solution_and_copy_checker
 import os
 import codecs
 import shutil
+import config
 
 class JavaTask(CompilableTask):
 
   def __init__(self):
     CompilableTask.__init__(self, create_files = create_solution_and_copy_checker)
-    self.execute_string = "java -Xmx64m -cp {0} Checker"
+    self.execute_string = "java -Djava.security.manager -Xmx140m -Djava.security.policy={0}/java.policy -cp {{0}} Checker".format(config.PLAYGROUND_FILES_PATH)
     self.checker_file = "Checker.java"
     self.filename = "Solution.java"
     self.compile_strings = ["javac {0}/Solution.java", "javac -cp {0} {0}/Checker.java"]
